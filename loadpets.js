8227e8f17df03e0a9e125155bbbd1324211c4bc1
Chimix <lavireak9@gmail.com>
2025-06-11 20:36:51 +0700

    add 4x2 grid in the all pets

--- loadpets.js
+++ loadpets.js
@@ -11,9 +11,42 @@ const pets = [
         "age": 3,
         "img": "img/dogs/dog02.jpg"
     },
-    { "name": "Whiskers", "type": "Cat", "age": 2, "img": "img/cats/cat01.jpg" },
-    { "name": "Mittens", "type": "Cat", "age": 2, "img": "img/cats/cat02.jpg" },
-
+    { 
+        "name": "Whiskers", 
+        "type": "Cat", 
+        "age": 2, 
+        "img": "img/cats/cat01.jpg" 
+    },
+    { 
+        "name": "Mittens", 
+        "type": "Cat", 
+        "age": 4,
+        "img": "img/cats/cat02.jpg"
+    },
+    {
+        "name": "Max",
+        "type": "bird",
+        "age": 5,
+        "img": "img/birds/bird01.jpg"
+    },
+    {
+        "name": "Luna",
+        "type": "bird",
+        "age": 1,
+        "img": "img/birds/bird02.jpg"
+    },
+    {
+        "name": "Charlie",
+        "type": "capybara",
+        "age": 2,
+        "img": "img/capybaras/capybara01.jpg"
+    },
+    {
+        "name": "Bella",
+        "type": "capybara",
+        "age": 3,
+        "img": "img/capybaras/capybara02.jpg"
+    }
 ]
 
 for(let i = 0; i < pets.length; i++) {
